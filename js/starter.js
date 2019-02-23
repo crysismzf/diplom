b5c08f463ebf6f4689b34f17ea82b636228d60b3
petrushenko <stas-petrushenko@mail.ru>
2019-02-23 19:43:26 +0300

    добавлена страница с фото из альбома

--- js/starter.js
+++ js/starter.js
@@ -1,6 +1,16 @@
-$("h1").hide();
+$(".text_for_header").hide();
 $("nav").hide();
-$(function() {
-	$("h1").fadeIn(3000);
+$(function(e) {
+	$(".text_for_header").fadeIn(3000);
 	$("nav").fadeIn(3000);
+    // $("nav").mouseout(MouserOverNav);
 });
+
+// function MouserOverNav() {
+// 	var el = this;
+// 	if (el.id!==undefined) {
+//         $(el).css("width","20%")
+// 	} else {
+//         $(el).css("width","20%")
+// 	}
+// };
